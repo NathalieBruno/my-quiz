da51c441aeba0d352d1a2a9674b4fb2e228aa63e
NathalieBruno <144827786+NathalieBruno@users.noreply.github.com>
2024-12-04 20:13:38 +0100

    dark-mode fix and added start btn function

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 let darkMode = document.querySelector(".dark-mode-toggle");
+let darkModeToggle = document.querySelector("#dark-mode");
 
 darkMode.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode-toggle");
-  darkMode.textContent = document.body.classList.contains("dark-mode-toggle") ? "Light theme 🌞 " : "Dark theme 😎 ";
+  document.body.classList.toggle("dark-mode");
+  darkModeToggle.textContent = document.body.classList.contains("dark-mode") ? "Light theme 🌞 " : "Dark theme 😎";
+});
+
+const quizQuestions = [
+  {
+    question: "Vilken färg har solen?",
+    answers: [
+      { text: "grön", correct: false },
+      { text: "lila", correct: false },
+      { text: "gul", correct: true },
+    ],
+  },
+];
+
+let currentQuestionIndex = 0;
+let startBtn = document.querySelector("#start-btn");
+
+startBtn.addEventListener("click", () => {
+  document.querySelector("#start-page").style.display = "none";
+  document.querySelector(".main-container").style.display = "block";
+  showQuestion(currentQuestionIndex);
 });
